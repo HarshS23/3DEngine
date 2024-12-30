02115d8062763766c64dce4fef5aa170ae0aefcb
Adharsh Shokkalingam <adharsh@Adharshs-MacBook-Air.local>
2024-12-29 21:17:07 -0700

    Vector files made

---
+++ vector.c
@@ -0,0 +1,60 @@
+#include "vector.h"
+
+Vec3 add_v3(Vec3 v1, Vec3 v2){
+    Vec3 output = {0,0,0};
+
+    output.x = v1.x + v2.x;
+    output.y = v1.y + v2.y;
+    output.z = v1.z + v2.z;
+
+    return output;  
+}
+
+Vec3 sub_v3(Vec3 v1, Vec3 v2){
+    Vec3 output = {0,0,0};
+
+    output.x = v1.x - v2.x;
+    output.y = v1.y - v2.y;
+    output.z = v1.z - v2.z;
+
+    return output;  
+    
+}
+
+Vec3 mul_v3(Vec3 v,  int x){
+    Vec3 output = {0,0,0};
+
+    output.x *= x; 
+    output.y *= x; 
+    output.z *= x;
+
+    return output;
+}
+
+Vec3 div_v3(Vec3 v ,  int x){
+    Vec3 output = {0,0,0};
+
+    output.x /= x; 
+    output.y /= x; 
+    output.z /= x;
+
+    return output;
+
+}
+
+float dot_v3(Vec3 v1, Vec3 v2){
+    return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+}
+
+float len_v3(Vec3 v){
+    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+Vec3 normalize_v3(Vec3 v1){
+    float mag = len_v3(v1);
+    if (mag == 0){
+        return (Vec3){0,0,0};
+    }else{
+        return mul_v3(v1, 1.0f);
+    }
+}
---
+++ vector.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// 3 dimensional Vector data type 
+typedef struct{
+    float x; 
+    float y; 
+    float z;
+}Vec3;
+
+Vec3 add_v3(Vec3 v1, Vec3 v2);
+Vec3 sub_v3(Vec3 v1, Vec3 v2);
+Vec3 mul_v3(Vec3 v, int x);
+Vec3 div_v3(Vec3 v, int x);
+float dot_v3(Vec3 v1, Vec3 v2);
+float len_v3(Vec3 v);
+Vec3 normalize_v3(v1);
